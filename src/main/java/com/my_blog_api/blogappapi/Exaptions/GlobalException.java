0cf6890b250b45d93f38d894e4ef5edbc0e7161d
sarvesh <lucksam38@gmail.com>
2024-09-09 22:25:25 +0530

    marge code

--- src/main/java/com/my_blog_api/blogappapi/Exaptions/GlobalException.java
+++ src/main/java/com/my_blog_api/blogappapi/Exaptions/GlobalException.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
-import java.nio.file.AccessDeniedException;
 import java.util.HashMap;
 import java.util.Map;
 
